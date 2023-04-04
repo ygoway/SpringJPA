d5ce6e79fb22fcd989c3a2053691765c58560fb5
Andrii <velandre375@gmail.com>
2023-04-04 20:47:51 +0300

    reworked reuse code

--- src/main/java/com/example/hw6maven/service/UserService.java
+++ src/main/java/com/example/hw6maven/service/UserService.java
@@ -2,20 +2,20 @@ package com.example.hw6maven.service;
 
 import com.example.hw6maven.entity.User;
 import com.example.hw6maven.repository.UserRepository;
+import com.example.hw6maven.service.validation.ValidationImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Scanner;
 
-import static com.example.hw6maven.service.validation.Validations.EMAIL_VALIDATION;
-import static com.example.hw6maven.service.validation.Validations.ONLY_NUMERIC;
-
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ValidationImpl validation;
 
     public void createUser() {
         Scanner scanner = new Scanner(System.in);
@@ -24,13 +24,8 @@ public class UserService {
         System.out.println("Enter your lastname");
         String lastName = scanner.nextLine();
         System.out.println("Enter your email");
-        String email;
-        do {
-            email = scanner.nextLine();
-            if (!email.matches(EMAIL_VALIDATION)) {
-                System.out.println("Invalid user email, pls try again");
-            }
-        } while (!email.matches(EMAIL_VALIDATION));
+        String email = scanner.nextLine();
+        validation.emailValidation(email);
         System.out.println("Enter your age");
         int age = scanner.nextInt();
 
@@ -50,40 +45,22 @@ public class UserService {
     public void getUserByEmail() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter email");
-        String email;
-        do {
-            email = scanner.nextLine();
-            if (!email.matches(EMAIL_VALIDATION)) {
-                System.out.println("Invalid user email, pls try again");
-            }
-        } while (!email.matches(EMAIL_VALIDATION));
+        String email = scanner.nextLine();
+        validation.emailValidation(email);
         System.out.println(userRepository.getByEmail(email));
     }
 
     public void updateUserById() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your ID");
-        Long id;
-        String stringID;
-        do {
-            stringID = scanner.nextLine();
-            if (!stringID.matches(ONLY_NUMERIC)) {
-                System.out.println("Invalid ID format, try again!");
-            }
-            id = Long.parseLong(stringID);
-        } while (!stringID.matches(ONLY_NUMERIC));
+        String textID = scanner.nextLine();
         System.out.println("Change ur name");
         String fistName = scanner.nextLine();
         System.out.println("Change ur lastname");
         String lastName = scanner.nextLine();
         System.out.println("Change ur email");
-        String email;
-        do {
-            email = scanner.nextLine();
-            if (!email.matches(EMAIL_VALIDATION)) {
-                System.out.println("Invalid user email, pls try again");
-            }
-        } while (!email.matches(EMAIL_VALIDATION));
+        String email = scanner.nextLine();
+        validation.emailValidation(email);
         System.out.println("Change ur age");
         int age = scanner.nextInt();
 
@@ -91,7 +68,7 @@ public class UserService {
         if (!userEmail.isEmpty()) {
             System.out.println("User with email " + email + " already exist!");
         } else {
-            User user = userRepository.updateUserById(id, fistName, lastName, email, age);
+            User user = userRepository.updateUserById(validation.idValidation(textID), fistName, lastName, email, age);
             System.out.println("Successful changes : " + user);
         }
     }
@@ -99,30 +76,14 @@ public class UserService {
     public void deleteUserById() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your ID");
-        Long id;
-        String stringID;
-        do {
-            stringID = scanner.nextLine();
-            if (!stringID.matches(ONLY_NUMERIC)) {
-                System.out.println("Invalid ID format, try again!");
-            }
-            id = Long.parseLong(stringID);
-        } while (!stringID.matches(ONLY_NUMERIC));
-        userRepository.removeUserByID(id);
+        String textID = scanner.nextLine();
+        userRepository.removeUserByID(validation.idValidation(textID));
     }
 
     public void getUserById() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your ID");
-        Long id;
-        String stringID;
-        do {
-            stringID = scanner.nextLine();
-            if (!stringID.matches(ONLY_NUMERIC)) {
-                System.out.println("Invalid ID format, try again!");
-            }
-            id = Long.parseLong(stringID);
-        } while (!stringID.matches(ONLY_NUMERIC));
-        System.out.println(userRepository.getUserById(id));
+        String textID = scanner.nextLine();
+        System.out.println(userRepository.getUserById(validation.idValidation(textID)));
     }
 }
