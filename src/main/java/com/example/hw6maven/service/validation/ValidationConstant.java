c0724c41755b6db190ec0052402b91d270f1df32
Andrii <velandre375@gmail.com>
2023-04-04 20:44:18 +0300

    rename class

--- src/main/java/com/example/hw6maven/service/validation/ValidationConstant.java
+++ src/main/java/com/example/hw6maven/service/validation/ValidationConstant.java
@@ -1,6 +1,6 @@
 package com.example.hw6maven.service.validation;
 
-public class Validations {
+public class ValidationConstant {
 
     public static final String EMAIL_VALIDATION = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}\\b";
     public static final String ONLY_NUMERIC = "[0-9]+";
